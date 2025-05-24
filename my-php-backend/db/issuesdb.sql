7678d871003329dfa3c20ef31afea8a6268eccb6
diana <dianamariamartisca@gmail.com>
2025-05-24 03:35:33 +0300

    added status in requests db

--- my-php-backend/db/issuesdb.sql
+++ my-php-backend/db/issuesdb.sql
@@ -31,7 +31,8 @@ CREATE TABLE `requests` (
   `date_requested` datetime NOT NULL, 
   `description` text NOT NULL, 
   `images` text DEFAULT NULL, 
-  `response` text DEFAULT NULL, 
+  `response` text DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'in asteptare', 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
